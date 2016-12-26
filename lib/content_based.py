164466f6c94abc3220758507c41e12fde2f1fd1e
Mostafa Mahmoud <mostafa.amin93@gmail.com>
2016-12-26 04:13:31 +0100

    RecommenderConfiguration and the Abstract RecommenderSystem and ContentBased. Added tests for all of them.

--- lib/content_based.py
+++ lib/content_based.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python
 """
-This module provides the functionalities of content-based analysis of the
-tests.
+This module provides the functionalities of content-based analysis of the tests.
 """
+import numpy
 
 
 class ContentBased(object):
     """
-    A class that will take the parsed data, and returns a distribution of
-    the content-based information.
+    An abstract class that will take the parsed data, and returns a distribution of the content-based information.
     """
-    def __init__(self, data):
+    def __init__(self, ratings, n_factors):
         """
         Constructor of ContentBased processor.
         """
-        pass
+        self.n_factors = n_factors
+        self.n_items = ratings.shape[0]
+        self.ratings = ratings
 
-    def process(self):
+    def train(self, num_iterations=10):
         """
-        Start processing the data.
+        Train the content-based.
+        @param num_iterations(int): number of iterations of the training.
         """
-        pass
+        self.distribution = numpy.random.random((self.n_items, self.n_factors))
 
     def get_word_distribution(self):
         """
         @returns A matrix of the words x topics distribution.
         """
-        pass
+        return self.distribution
--- lib/recommender_system.py
+++ lib/recommender_system.py
@@ -1,25 +1,47 @@
 #!/usr/bin/env python
 """
-This is a module that contains the main class and functionalities of
-the recommender systems.
+This is a module that contains the main class and functionalities of the recommender systems.
 """
+import numpy
+from content_based import ContentBased
+from util.data_parser import DataParser
+from util.recommender_configuer import RecommenderConfiguration
 
 
 class RecommenderSystem(object):
     """
-    A class that will combine the content-based and user-based method,
+    A class that will combine the content-based and collaborative-filtering,
     in order to provide the main functionalities of recommendations.
     """
-    def __init__(self, data):
+    def __init__(self, n_factors=4):
         """
         Constructor of the RecommenderSystem.
         """
-        pass
+        # DataParser.process()
+        ratings = numpy.matrix(DataParser.get_ratings_matrix())
+        self.config = RecommenderConfiguration()
+        if self.config.get_content_based() == 'LDA':
+            pass
+        if self.config.get_content_based() == 'LDA2Vec':
+            pass
+        # TODO: get vocabulary matrix for content based.
+        self.content_based = ContentBased(ratings, n_factors)
+        self.hyperparameters = self.config.get_hyperparameters()
+        # TODO: write the evaluator class, and hyperparameters for the algorithms.
+        # self.collaborative_filtering = CollaborativeFiltering(ratings, n_factors,
+        #                                                       self.hyperparameters['collaborative-filtering-lambda'])
+        # self.evaluator = Evaluator(self.config.get_error_metric())
 
     def process(self):
         """
-        Process the algorithm on the given data.
+        Process an iteration of the algorithm on the given data.
         """
+        self.content_based.train()
+        theta = self.content_based.get_word_distribution()
+        # TODO: Use collaborative filtering and evaluator
+        # u, v = self.collaborative_filtering.train(theta)
+        # error = self.evaluator.process(u, v)
+        # return error
 
     def recommend_items(self, user_id, num_recommendations=10):
         """
