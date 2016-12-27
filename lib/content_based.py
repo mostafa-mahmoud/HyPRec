0afa3a3bd942f5d5cc1deaeb68bc8f303734f1e0
Mostafa Mahmoud <mostafa.amin93@gmail.com>
2016-12-27 19:40:13 +0100

    Integrated evaluator class, and raising error for invalid configuration.

--- lib/content_based.py
+++ lib/content_based.py
@@ -9,20 +9,22 @@ class ContentBased(object):
     """
     An abstract class that will take the parsed data, and returns a distribution of the content-based information.
     """
-    def __init__(self, ratings, n_factors):
+    def __init__(self, ratings, n_factors, n_iterations=10):
         """
         Constructor of ContentBased processor.
         """
         self.n_factors = n_factors
         self.n_items = ratings.shape[0]
         self.ratings = ratings
+        self.n_iterations = n_iterations
 
-    def train(self, num_iterations=10):
+    def train(self):
         """
         Train the content-based.
-        @param num_iterations(int): number of iterations of the training.
         """
         self.distribution = numpy.random.random((self.n_items, self.n_factors))
+        for _ in range(self.n_iterations):
+            pass
 
     def get_word_distribution(self):
         """
--- lib/recommender_system.py
+++ lib/recommender_system.py
@@ -4,6 +4,7 @@ This is a module that contains the main class and functionalities of the recomme
 """
 import numpy
 from content_based import ContentBased
+from evaluator import Evaluator
 from util.data_parser import DataParser
 from util.recommender_configuer import RecommenderConfiguration
 
@@ -13,24 +14,31 @@ class RecommenderSystem(object):
     A class that will combine the content-based and collaborative-filtering,
     in order to provide the main functionalities of recommendations.
     """
-    def __init__(self, n_factors=4):
+    def __init__(self):
         """
         Constructor of the RecommenderSystem.
         """
         # DataParser.process()
-        ratings = numpy.matrix(DataParser.get_ratings_matrix())
+        self.ratings = numpy.matrix(DataParser.get_ratings_matrix())
         self.config = RecommenderConfiguration()
+        self.n_factors = self.config.get_hyperparameters()['n_factors']
+        self.n_iterations = self.config.get_options()['n_iterations']
         if self.config.get_content_based() == 'LDA':
             pass
-        if self.config.get_content_based() == 'LDA2Vec':
+        elif self.config.get_content_based() == 'LDA2Vec':
             pass
+        else:
+            raise NameError("Not a valid contend based " + self.config.get_content_based())
         # TODO: get vocabulary matrix for content based.
-        self.content_based = ContentBased(ratings, n_factors)
+        self.content_based = ContentBased(self.ratings, self.n_factors, self.n_iterations)
         self.hyperparameters = self.config.get_hyperparameters()
-        # TODO: write the evaluator class, and hyperparameters for the algorithms.
-        # self.collaborative_filtering = CollaborativeFiltering(ratings, n_factors,
+        # TODO: write the hyperparameters for the algorithms.
+        # self.collaborative_filtering = CollaborativeFiltering(ratings, self.n_factors,
         #                                                       self.hyperparameters['collaborative-filtering-lambda'])
-        # self.evaluator = Evaluator(self.config.get_error_metric())
+        if self.config.get_error_metric() == 'RMS':
+            self.evaluator = Evaluator(self.ratings)
+        else:
+            raise NameError("Not a valid error metric " + self.config.get_error_metric())
 
     def process(self):
         """
@@ -40,11 +48,14 @@ class RecommenderSystem(object):
         theta = self.content_based.get_word_distribution()
         # TODO: Use collaborative filtering and evaluator
         # u, v = self.collaborative_filtering.train(theta)
-        # error = self.evaluator.process(u, v)
-        # return error
+        error = self.evaluator.get_rmse(theta)
+        return error
 
     def recommend_items(self, user_id, num_recommendations=10):
         """
-        @return a list of the best recommendations for a given user_id.
+        Get recommendations for a user.
+        @param(int) user_id: The id of the user.
+        @param(int) num_recommendations: The number of recommended items.
+        @returns(list) a list of the best recommendations for a given user_id.
         """
         pass
