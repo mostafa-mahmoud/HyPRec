e55dce5ce470c8674e2c224d7b12a5ef3c746d47
Omar Nada <ndomar.14@gmail.com>
2016-12-27 20:18:12 +0100

    changed the file to .py

---
+++ lib/abstract_recommender.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+class AbstractRecommender(object):
+	"""
+	A class that acts like an interface,
+	it is never initialized but the uv_decomposition
+	and content_based should implement it's methods
+	"""
+
+	def __init__(self):
+		raise NotImplementedError("Can't initialize this class")
+
+	def train(self):
+		raise NotImplementedError("Can't call this method")
+
+	def split(self):
+		raise NotImplementedError("Can't call this method")
+
+	def set_config(self):
+		raise NotImplementedError("Can't call this method")
