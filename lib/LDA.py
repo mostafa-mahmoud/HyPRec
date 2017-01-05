3b0a5d03e7c5bfec745ffc19353f6357f5e09ac9
Mostafa Mahmoud <mostafa.amin93@gmail.com>
2017-01-05 18:02:18 +0100

    Documentation for LDA.

--- lib/LDA.py
+++ lib/LDA.py
@@ -6,11 +6,18 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 
 class LDARecommender(ContentBased):
+    """
+    LDA Recommender, a content based recommender that uses LDA.
+    """
     def __init__(self, abstracts, evaluator, config, verbose=False):
         super(LDARecommender, self).__init__(abstracts, evaluator, config, verbose)
 
     def train(self, n_iter=5):
-        term_freq_vectorizer = CountVectorizer(max_df=0.95, min_df=2, stop_words='english', max_features=self.n_items)
+        """
+        Train LDA Recommender, and store the word_distribution.
+        """
+        term_freq_vectorizer = CountVectorizer(max_df=0.95, min_df=2, stop_words='english',
+                                               max_features=self.n_items)
         term_freq = term_freq_vectorizer.fit_transform(self.abstracts)
 
         lda = LatentDirichletAllocation(n_topics=self.n_factors, max_iter=n_iter,
@@ -18,11 +25,16 @@ class LDARecommender(ContentBased):
         self.word_distribution = lda.fit_transform(term_freq)
 
     def split(self):
+        """
+        split the data into train and test data.
+        @returns (tuple) A tuple of (train_data, test_data)
+        """
         return super(LDARecommender, self).split()
 
     def set_config(self, config):
         """
         set the hyperparamenters of the algorithm.
+        @param config(dict): A dictionary of the hyperparameters.
         """
         super(LDARecommender, self).set_config(config)
 
