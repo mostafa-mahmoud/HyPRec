a3efd2f61d8860b1ed6f0427ed31a4ce5dc9da85
Omar Nada <ndomar.14@gmail.com>
2017-01-08 19:09:24 +0100

    fixed the condition in runtests

--- runtests.py
+++ runtests.py
@@ -7,13 +7,13 @@ import os
 import unittest
 
 
-def runtests(test_args=None):
+def runtests(test_args):
     """
     Initialize a test suite that contains all the tests in the given modules
     in @test_args, and run the test suite. The modules are taken by command
     line arguments.
     """
-    if test_args is not None:
+    if len(test_args) == 0:
         base_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'tests')
         test_args = ['tests.' + os.path.splitext(f)[0] for f in os.listdir(base_dir) if os.path.isfile(os.path.join(base_dir, f))]
     suite = unittest.TestSuite()
