838eb272f751cd7b8b2e3d97fea9037793f9b216
Omar Nada <ndomar.14@gmail.com>
2017-01-08 22:14:42 +0100

    changed the way runnabes is parsing arguments

--- runnables.py
+++ runnables.py
@@ -4,6 +4,7 @@ A module to run different recommenders.
 """
 import numpy
 import sys
+from optparse import OptionParser
 from lib.evaluator import Evaluator
 from lib.collaborative_filtering import CollaborativeFiltering
 from lib.grid_search import GridSearch
@@ -92,24 +93,31 @@ class RunnableRecommenders(object):
         return error
 
 if __name__ == '__main__':
-    arguments = sys.argv[1:]
-    use_database = False
-    if len(arguments) >= 1:
-        if arguments[0] == 'true':
-            use_database = True
+    parser = OptionParser()
+    parser.add_option("-d", "--use-database", dest="db", action='store_true',
+                  help="use database to run the recommender", metavar="DB")
+    options, args = parser.parse_args()
+    use_database = options.db != None
     runnable = RunnableRecommenders(use_database)
-    if len(arguments) >= 2:
-        if arguments[1] == 'recommender':
+    found_runnable = False
+    for arg in args:
+        if arg == 'recommender':
             print(runnable.run_recommender())
-        elif arguments[1] == 'collaborative':
+            found_runnable = True
+        elif arg == 'collaborative':
             print(runnable.run_collaborative())
-        elif arguments[1] == 'grid_search':
+            found_runnable = True
+        elif arg == 'grid_search':
             print(runnable.run_grid_search())
-        elif arguments[1] == 'lda':
+            found_runnable = True
+        elif arg == 'lda':
             print(runnable.run_lda())
-        elif arguments[1] == 'lda2vec':
+            found_runnable = True
+        elif arg == 'lda2vec':
             print(runnable.run_lda2vec())
+            found_runnable = True
         else:
-            print(runnable.run_recommender())
-    else:
+            print("'%s' option is not valid, please use one of ['recommender', 'collaborative', 'grid_search', 'lda', 'lda2vec']" % arg)
+    if found_runnable == False:
+        print("Didn't find any valid option, running recommender instead.")
         print(runnable.run_recommender())
