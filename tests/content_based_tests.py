6f480ac5586ec26e32a9510f6a4d00f80c8868aa
Mostafa Mahmoud <mostafa.amin93@gmail.com>
2017-02-28 20:14:45 +0100

    Update tests for pearsons coefficient

--- tests/content_based_tests.py
+++ tests/content_based_tests.py
@@ -73,8 +73,6 @@ class TestContentBased(TestcaseBase):
         content_based.train()
         self.assertTrue(isinstance(content_based, AbstractRecommender))
         self.assertTrue(content_based.get_predictions().shape, (self.users, self.documents))
-        self.assertLessEqual(content_based.get_predictions().max(), 1.0 + 1e-6)
-        self.assertGreaterEqual(content_based.get_predictions().min(), -1e-6)
 
 
 class TestLDA(TestcaseBase):
@@ -88,8 +86,6 @@ class TestLDA(TestcaseBase):
         self.assertGreaterEqual(content_based.get_document_topic_distribution().min(), -1e-6)
         self.assertTrue(isinstance(content_based, AbstractRecommender))
         self.assertEqual(content_based.get_predictions().shape, (self.users, self.documents))
-        self.assertLessEqual(content_based.get_predictions().max(), 1.0 + 1e-6)
-        self.assertGreaterEqual(content_based.get_predictions().min(), -1e-6)
 
 
 class TestLDA2Vec(TestcaseBase):
@@ -103,8 +99,6 @@ class TestLDA2Vec(TestcaseBase):
         self.assertGreaterEqual(content_based.get_document_topic_distribution().min(), -1e-6)
         self.assertTrue(isinstance(content_based, AbstractRecommender))
         self.assertEqual(content_based.get_predictions().shape, (self.users, self.documents))
-        self.assertLessEqual(content_based.get_predictions().max(), 1.0 + 1e-6)
-        self.assertGreaterEqual(content_based.get_predictions().min(), -1e-6)
 
 
 class TestSDAE(TestcaseBase):
@@ -118,8 +112,6 @@ class TestSDAE(TestcaseBase):
         self.assertGreaterEqual(content_based.get_document_topic_distribution().min(), -1e-6)
         self.assertTrue(isinstance(content_based, AbstractRecommender))
         self.assertEqual(content_based.get_predictions().shape, (self.users, self.documents))
-        self.assertLessEqual(content_based.get_predictions().max(), 1.0 + 1e-6)
-        self.assertGreaterEqual(content_based.get_predictions().min(), -1e-6)
         encode, decode = content_based.get_cnn()
         term_freq = self.abstracts_preprocessor.get_term_frequency_sparse_matrix().todense()
         rand_term_freq = numpy.random.normal(term_freq, 0.25)
