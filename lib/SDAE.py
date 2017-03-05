c00958a44e39ed1a32803b3fc76fcd4de2437d90
Mostafa Mahmoud <mostafa.amin93@gmail.com>
2017-03-05 21:56:22 +0100

    Added loss function for the encoding layer in SDAE, learning from ratings, and support for save/load matrices.

--- lib/SDAE.py
+++ lib/SDAE.py
@@ -5,6 +5,7 @@ stacked denoising autoencoders.
 """
 import time
 import numpy
+from numpy.linalg import solve
 from overrides import overrides
 from keras import backend
 from keras.layers.convolutional import Convolution1D
@@ -37,24 +38,71 @@ class SDAERecommender(ContentBased):
                                               verbose, load_matrices, dump_matrices)
 
     @overrides
+    def set_hyperparameters(self, hyperparameters):
+        """
+        Set the  of the algorithm. Namely n_factors, _lambda.
+
+        :param dict hyperparameters: A dictionary of the hyperparameters.
+        """
+        self.n_factors = hyperparameters['n_factors']
+        self._lambda = hyperparameters['_lambda']
+        self.hyperparameters = hyperparameters.copy()
+
+    @overrides
+    def train_k_fold(self):
+        """
+        Trains the k folds of collaborative filtering.
+        """
+        all_errors = []
+        for current_k in range(self.k_folds):
+            self.train_data, self.test_data = self.evaluator.get_fold(current_k, self.fold_train_indices,
+                                                                      self.fold_test_indices)
+            self.hyperparameters['fold'] = current_k
+            current_error = self.train_one_fold()
+            all_errors.append(current_error)
+            self.predictions = None
+        return numpy.mean(all_errors, axis=0)
+
+    @overrides
     def train_one_fold(self):
         """
-        Try to load saved matrix if load_matrices is false, else train
+        Train model for n_iter iterations from scratch.
         """
-        matrix_found = False
-        if self._load_matrices is True:
-            matrix_shape = (self.n_items, self.n_factors)
-            matrix_found, matrix = self.initializer.load_matrix(self.hyperparameters,
-                                                                'document_distribution_sdae', matrix_shape)
-            self.document_distribution = matrix
-            if self._verbose and matrix_found:
-                print("Document distribution was set from file, will not train.")
-        if matrix_found is False:
+        matrices_found = False
+        if self._load_matrices is False:
+            self.user_vecs = numpy.random.random((self.n_users, self.n_factors))
+            self.item_vecs = numpy.random.random((self.n_items, self.n_factors))
+        else:
+            users_found, self.user_vecs = self.initializer.load_matrix(self.hyperparameters,
+                                                                       'user_mat', (self.n_users, self.n_factors))
+            if self._verbose and users_found:
+                print("User distributions files were found.")
+            items_found, self.item_vecs = self.initializer.load_matrix(self.hyperparameters, 'item_mat',
+                                                                       (self.n_items, self.n_factors))
+            if self._verbose and items_found:
+                print("Document distributions files were found.")
+
+            docs_found, self.document_distribution = self.initializer.load_matrix(self.hyperparameters, 'document_distribution_sdae',
+                                                                                  (self.n_items, self.n_factors))
+            if self._verbose and docs_found:
+                print("Document latent distributions files were found.")
+            matrices_found = users_found and items_found and docs_found
+
+        if not matrices_found:
             if self._verbose and self._load_matrices:
-                print("Document distribution file was not found. Will train SDAE.")
+                print("User and Document distributions files were not found, will train collaborative.")
             self._train()
-            if self._dump_matrices:
-                self.initializer.save_matrix(self.document_distribution, 'document_distribution_sdae')
+        else:
+            if self._verbose and self._load_matrices:
+                print("User and Document distributions files found, will not train the model further.")
+
+        if self._dump_matrices:
+            self.initializer.set_config(self.hyperparameters, self.n_iter)
+            self.initializer.save_matrix(self.user_vecs, 'user_mat')
+            self.initializer.save_matrix(self.item_vecs, 'item_mat')
+            self.initializer.save_matrix(self.document_distribution, 'document_distribution_sdae')
+
+        return self.get_evaluation_report()
 
     def get_cnn(self):
         """
@@ -80,6 +128,7 @@ class SDAERecommender(ContentBased):
         model.add(Activation('softmax'))
         model.add(Reshape((self.n_factors,), name='encoding'))
         intermediate = Model(input=model.input, output=model.get_layer('encoding').output)
+        intermediate.compile(loss='mean_squared_error', optimizer='sgd')
 
         model.add(Reshape((1, self.n_factors)))
         model.add(Convolution1D(n2, 3, border_mode='same'))
@@ -98,27 +147,83 @@ class SDAERecommender(ContentBased):
         model.compile(loss='mean_squared_error', optimizer='sgd')
         return intermediate, model
 
+    def uv_step(self, latent_vectors, fixed_vecs, ratings, _lambda, type='user'):
+        """
+        The function computes only one step in the ALS algorithm
+
+        :param ndarray latent_vectors: the vector to be optimized
+        :param ndarray fixed_vecs: the vector to be fixed
+        :param ndarray ratings: ratings that will be used to optimize latent * fixed
+        :param float _lambda: reguralization parameter
+        :param str type: either user or item.
+        """
+        if type == 'user':
+            # Precompute
+            YTY = fixed_vecs.T.dot(fixed_vecs)
+            lambdaI = numpy.eye(YTY.shape[0]) * _lambda
+
+            for u in range(latent_vectors.shape[0]):
+                latent_vectors[u, :] = solve((YTY + lambdaI),
+                                             ratings[u, :].dot(fixed_vecs))
+        elif type == 'item':
+            # Precompute
+            XTX = fixed_vecs.T.dot(fixed_vecs)
+            lambdaI = numpy.eye(XTX.shape[0]) * _lambda
+            for i in range(latent_vectors.shape[0]):
+                if self.document_distribution is not None:
+                    latent_vectors[i, :] = solve((XTX + lambdaI),
+                                                 ratings[:, i].T.dot(fixed_vecs) +
+                                                 self.document_distribution[i, :] * _lambda)
+                else:
+                    latent_vectors[i, :] = solve((XTX + lambdaI), ratings[:, i].T.dot(fixed_vecs))
+        return latent_vectors
+
     def _train(self):
         """
         Train the stacked denoising autoencoders.
         """
-        encode_cnn, cnn = self.get_cnn()
-        if self._verbose:
-            "CNN is constructed..."
+        if 'fold' in self.hyperparameters:
+            current_fold = self.hyperparameters['fold'] + 1
+        else:
+            current_fold = 0
         term_freq = self.abstracts_preprocessor.get_term_frequency_sparse_matrix().todense()
         rand_term_freq = numpy.random.normal(term_freq, 0.25)
+        encode_cnn, cnn = self.get_cnn()
+        if self._verbose:
+            print("CNN is constructed...")
         iterations = 0
         batchsize = 2048
         for epoch in range(1, 1 + self.n_iter):
-            for inp_batch, out_batch in chunks(batchsize, rand_term_freq, term_freq):
+            t0 = time.time()
+            self.document_distribution = encode_cnn.predict(term_freq)
+            self.user_vecs = self.uv_step(self.user_vecs, self.item_vecs, self.train_data, self._lambda, type='user')
+            self.item_vecs = self.uv_step(self.item_vecs, self.user_vecs, self.train_data, self._lambda, type='item')
+            t1 = time.time()
+            iterations += 1
+            if self._verbose:
+                error = self.evaluator.get_rmse(self.user_vecs.dot(self.item_vecs.T), self.train_data)
+                if current_fold == 0:
+                    logs = dict(it=iterations, epoch=epoch, loss=error, time=(t1 - t0))
+                    print('Iteration:{it:05d} Epoch:{epoch:02d} Loss:{loss:1.4e} Time:{time:.3f}s'.format(**logs))
+                else:
+                    logs = dict(fold=current_fold, it=iterations, epoch=epoch, loss=error, time=(t1 - t0))
+                    print('Fold:{fold:02d} Iteration:{it:05d} Epoch:{epoch:02d} Loss:{loss:1.4e} Time:{time:.3f}s'.format(**logs))
+
+            for inp_batch, out_batch, it_batch in chunks(batchsize, rand_term_freq, term_freq, self.item_vecs):
                 t0 = time.time()
-                l = cnn.train_on_batch(inp_batch, out_batch)
+                l1 = encode_cnn.train_on_batch(inp_batch, it_batch)
+                l2 = cnn.train_on_batch(inp_batch, out_batch)
                 t1 = time.time()
                 iterations += 1
                 if self._verbose:
-                    msg = "Iteration:{it:05d} Epoch:{epoch:02d} Loss:{loss:1.3e} Time:{tim:.3f}s"
-                    logs = dict(loss=float(l), epoch=epoch, it=iterations, tim=(t1 - t0))
-                    print(msg.format(**logs))
+                    if current_fold == 0:
+                        msg = "Iteration:{it:05d} Epoch:{epoch:02d} LossR:{loss:1.3e} LossE:{lossid:1.3e} Time:{tim:.3f}s"
+                        logs = dict(loss=float(l2), lossid=float(l1), epoch=epoch, it=iterations, tim=(t1 - t0))
+                        print(msg.format(**logs))
+                    else:
+                        msg = "Fold:{fold:02d} Iteration:{it:05d} Epoch:{epoch:02d} LossR:{loss:1.3e} LossE:{lossid:1.3e} Time:{tim:.3f}s"
+                        logs = dict(fold=current_fold, loss=float(l2), lossid=float(l1), epoch=epoch, it=iterations, tim=(t1 - t0))
+                        print(msg.format(**logs))
 
         self.document_distribution = encode_cnn.predict(term_freq)
         rms = cnn.evaluate(term_freq, term_freq)
@@ -130,3 +235,23 @@ class SDAERecommender(ContentBased):
         if self._verbose:
             print("SDAE trained...")
         return rms
+
+    @overrides
+    def get_predictions(self):
+        """
+        Predict ratings for every user and item.
+
+        :returns: A (user, document) matrix of predictions
+        :rtype: ndarray
+        """
+        return self.user_vecs.dot(self.item_vecs.T)
+
+    @overrides
+    def predict(self, user, item):
+        """
+        Single user and item prediction.
+
+        :returns: prediction score
+        :rtype: float
+        """
+        return self.user_vecs[user, :].dot(self.item_vecs[item, :].T)
