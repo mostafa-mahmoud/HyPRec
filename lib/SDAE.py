aa450a39da3dc0c963d67489a8b4f095fc81d7f6
Mostafa Mahmoud <mostafa.amin93@gmail.com>
2017-04-18 22:29:37 +0200

    Fixing SDAE old version

--- lib/SDAE.py
+++ lib/SDAE.py
@@ -5,7 +5,6 @@ stacked denoising autoencoders.
 """
 import time
 import numpy
-from numpy.linalg import solve
 from overrides import overrides
 from keras import backend
 from keras.layers.convolutional import Convolution1D
@@ -66,32 +65,6 @@ class SDAERecommender(CollaborativeFiltering, ContentBased):
         else:
             self.fold_train_indices, self.fold_test_indices = self.evaluator.get_kfold_indices()
             return self.train_k_fold()
-
-    @overrides
-    def train_k_fold(self):
-        """
-        Trains the k folds of SDAE.
-        """
-        all_errors = []
-        for current_k in range(self.k_folds):
-            self.train_data, self.test_data = self.evaluator.get_fold(current_k, self.fold_train_indices,
-                                                                      self.fold_test_indices)
-            self.hyperparameters['fold'] = current_k
-            current_error = self.train_one_fold()
-            all_errors.append(current_error)
-            self.predictions = None
-        return numpy.mean(all_errors, axis=0)
-
-    @overrides
-    def set_hyperparameters(self, hyperparameters):
-        """
-        Set the  of the algorithm. Namely n_factors, _lambda.
-
-        :param dict hyperparameters: A dictionary of the hyperparameters.
-        """
-        self.n_factors = hyperparameters['n_factors']
-        self._lambda = hyperparameters['_lambda']
-        self.hyperparameters = hyperparameters.copy()
 
     @overrides
     def train_k_fold(self):
@@ -193,64 +166,6 @@ class SDAERecommender(CollaborativeFiltering, ContentBased):
         model.compile(loss='mean_squared_error', optimizer='sgd')
         return intermediate, model
 
-    def uv_step(self, latent_vectors, fixed_vecs, ratings, _lambda, type='user'):
-        """
-        The function computes only one step in the ALS algorithm
-
-        :param ndarray latent_vectors: the vector to be optimized
-        :param ndarray fixed_vecs: the vector to be fixed
-        :param ndarray ratings: ratings that will be used to optimize latent * fixed
-        :param float _lambda: reguralization parameter
-        :param str type: either user or item.
-        """
-        if type == 'user':
-            # Precompute
-            lambdaI = numpy.eye(self.hyperparameters['n_factors']) * _lambda
-            for u in range(latent_vectors.shape[0]):
-                confidence = self.build_confidence_matrix(u, 'user')
-                YTY = (fixed_vecs.T * confidence).dot(fixed_vecs)
-                latent_vectors[u, :] = solve((YTY + lambdaI),
-                                             (ratings[u, :] * confidence).dot(fixed_vecs))
-        elif type == 'item':
-            # Precompute
-            lambdaI = numpy.eye(self.hyperparameters['n_factors']) * _lambda
-            for i in range(latent_vectors.shape[0]):
-                confidence = self.build_confidence_matrix(i, 'item')
-                XTX = (fixed_vecs.T * confidence).dot(fixed_vecs)
-                if self.document_distribution is not None:
-                    latent_vectors[i, :] = solve((XTX + lambdaI),
-                                                 (ratings[:, i].T * confidence).dot(fixed_vecs) +
-                                                 self.document_distribution[i, :] * _lambda)
-                else:
-                    latent_vectors[i, :] = solve((XTX + lambdaI), (ratings[:, i].T * confidence).dot(fixed_vecs))
-        return latent_vectors
-
-    def build_confidence_matrix(self, index, type='user'):
-        """
-        Builds a confidence matrix
-
-        :param int index: Index of the user or item to build confidence for.
-        :param str type: Type of confidence matrix, either user or item.
-
-        :returns: A confidence matrix
-        :rtype: ndarray
-        """
-        if type == 'user':
-            shape = self.item_vecs.shape[0]
-        else:
-            shape = self.user_vecs.shape[0]
-
-        confidence = numpy.array([0.1] * shape)
-        for i in range(len(confidence)):
-            if type == 'user':
-                if self.train_data[index][i] == 1:
-                    confidence[i] = 1
-            else:
-                if self.train_data[i][index] == 1:
-                    confidence[i] = 1
-
-        return confidence
-
     def _train(self):
         """
         Train the stacked denoising autoencoders.
@@ -273,14 +188,6 @@ class SDAERecommender(CollaborativeFiltering, ContentBased):
             t0 = time.time()
             self.user_vecs = self.als_step(self.user_vecs, self.item_vecs, self.train_data, self._lambda, type='user')
             self.item_vecs = self.als_step(self.item_vecs, self.user_vecs, self.train_data, self._lambda, type='item')
-
-        iterations = 0
-        batchsize = 2048
-        for epoch in range(1, 1 + self.n_iter):
-            t0 = time.time()
-            self.document_distribution = encode_cnn.predict(term_freq)
-            self.user_vecs = self.uv_step(self.user_vecs, self.item_vecs, self.train_data, self._lambda, type='user')
-            self.item_vecs = self.uv_step(self.item_vecs, self.user_vecs, self.train_data, self._lambda, type='item')
             t1 = time.time()
             iterations += 1
             if self._verbose:
@@ -317,7 +224,6 @@ class SDAERecommender(CollaborativeFiltering, ContentBased):
                     print("Local Optimum was found in the last iteration, breaking.")
                 break
 
-
         self.document_distribution = encode_cnn.predict(term_freq)
         rms = cnn.evaluate(term_freq, term_freq)
 
@@ -328,23 +234,3 @@ class SDAERecommender(CollaborativeFiltering, ContentBased):
         if self._verbose:
             print("SDAE trained...")
         return rms
-
-    @overrides
-    def get_predictions(self):
-        """
-        Predict ratings for every user and item.
-
-        :returns: A (user, document) matrix of predictions
-        :rtype: ndarray
-        """
-        return self.user_vecs.dot(self.item_vecs.T)
-
-    @overrides
-    def predict(self, user, item):
-        """
-        Single user and item prediction.
-
-        :returns: prediction score
-        :rtype: float
-        """
-        return self.user_vecs[user, :].dot(self.item_vecs[item, :].T)
